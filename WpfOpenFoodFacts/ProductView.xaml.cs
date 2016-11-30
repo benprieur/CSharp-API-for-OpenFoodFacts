82f99cfd3b7d10620835a9d80967c9c456670cb3
benoit <ben.prieur@laposte.net>
2016-11-30 17:08:17 +0100

    +++

--- WpfOpenFoodFacts/ProductView.xaml.cs
+++ WpfOpenFoodFacts/ProductView.xaml.cs
@@ -1,10 +1,10 @@
 ﻿using OpenFoodFactsAPI;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media.Imaging;
 
 namespace WpfOpenFoodFacts
 {
@@ -13,6 +13,9 @@ namespace WpfOpenFoodFacts
     /// </summary>
     public partial class ProductView : Window
     {
+        public const string _URL = "_url";
+        public const string TYPE_IMG = "type_img";
+         
         public Product Product { get; set;}
 
         public ProductView()
@@ -29,6 +32,11 @@ namespace WpfOpenFoodFacts
                 string propName = prop.Name;
                 PropertyInfo property = typeof(Product).GetProperty(propName);
 
+                ////////////////////////////////////////////
+                //
+                // typeof(string)
+                //
+                /////////////////////////////////////////////
                 if (prop.PropertyType == typeof(string))
                 {
                     object val = property.GetValue(Product);
@@ -36,15 +44,42 @@ namespace WpfOpenFoodFacts
 
                     TextBlock tb = new TextBlock();
                     tb.FontWeight = FontWeights.Bold;
+                    tb.TextWrapping = TextWrapping.Wrap;
                     tb.Text = propName + ": ";
                     wp1.Children.Add(tb);
 
-                    TextBox tbVal = new TextBox();
-                    tbVal.TextWrapping = TextWrapping.Wrap;
-                    tbVal.MaxWidth = 250;
-                    tbVal.Text = result;
-                    wp1.Children.Add(tbVal);
+                    // Type
+                    string type = "";
+                    if (propName.Length > _URL.Length)
+                    {
+                        if (propName.Substring(propName.Length - _URL.Length) == _URL)
+                        {
+                            type = TYPE_IMG;
+                        }
+                    }
+                    
+                    // Image
+                    if (type == TYPE_IMG)
+                    {
+                        Image imgVal = new Image();
+                        imgVal.Width = 125;
+                        imgVal.Source = new BitmapImage(new Uri(result));
+                        wp1.Children.Add(imgVal);
+                    }
+                    else
+                    { 
+                        TextBox tbVal = new TextBox();
+                        tbVal.TextWrapping = TextWrapping.Wrap;
+                        tbVal.MaxWidth = 250;
+                        tbVal.Text = result;
+                        wp1.Children.Add(tbVal);
+                    }
                 }
+                ////////////////////////////////////////////
+                //
+                // typeof(int)
+                //
+                ////////////////////////////////////////////
                 else if (prop.PropertyType == typeof(int))
                 {
                     object val = property.GetValue(Product);
@@ -52,6 +87,7 @@ namespace WpfOpenFoodFacts
 
                     TextBlock tb = new TextBlock();
                     tb.FontWeight = FontWeights.Bold;
+                    tb.TextWrapping = TextWrapping.Wrap;
                     tb.Text = propName + ": ";
                     wp2.Children.Add(tb);
 
@@ -61,22 +97,11 @@ namespace WpfOpenFoodFacts
                     tbVal.Text = result;
                     wp2.Children.Add(tbVal);
                 }
-                else if (prop.PropertyType == typeof(int))
-                {
-                    object val = property.GetValue(Product);
-                    string result = val.ToString();
-
-                    TextBlock tb = new TextBlock();
-                    tb.FontWeight = FontWeights.Bold;
-                    tb.Text = propName + ": ";
-                    wp2.Children.Add(tb);
-
-                    TextBox tbVal = new TextBox();
-                    tbVal.TextWrapping = TextWrapping.Wrap;
-                    tbVal.MaxWidth = 250;
-                    tbVal.Text = result;
-                    wp2.Children.Add(tbVal);
-                }
+                ////////////////////////////////////////////
+                //
+                // typeof(List<string>)
+                //
+                ////////////////////////////////////////////
                 else if (prop.PropertyType == typeof(List<string>))
                 {
                     List<string> val = (List<string>)property.GetValue(Product);
@@ -84,6 +109,7 @@ namespace WpfOpenFoodFacts
 
                     TextBlock tb = new TextBlock();
                     tb.FontWeight = FontWeights.Bold;
+                    tb.TextWrapping = TextWrapping.Wrap;
                     tb.Text = propName + ": ";
                     wp3.Children.Add(tb);
 
@@ -93,6 +119,11 @@ namespace WpfOpenFoodFacts
                     tbVal.Text = result;
                     wp3.Children.Add(tbVal);
                 }
+                ////////////////////////////////////////////
+                //
+                // typeof(NutrientLevels)
+                //
+                ////////////////////////////////////////////
                 else if (prop.PropertyType == typeof(NutrientLevels))
                 {
                     NutrientLevels nutrientLevels = (NutrientLevels)property.GetValue(Product);
@@ -101,6 +132,7 @@ namespace WpfOpenFoodFacts
                     {
                         TextBlock tb = new TextBlock();
                         tb.FontWeight = FontWeights.Bold;
+                        tb.TextWrapping = TextWrapping.Wrap;
                         tb.Text = propNL.Name + ": ";
                         wp4.Children.Add(tb);
 
@@ -113,6 +145,11 @@ namespace WpfOpenFoodFacts
                         wp4.Children.Add(tbVal);
                     }
                 }
+                ////////////////////////////////////////////
+                //
+                // typeof(Nutriments)
+                //
+                ////////////////////////////////////////////
                 else if (prop.PropertyType == typeof(Nutriments))
                 {
                     Nutriments nutrientLevels = (Nutriments)property.GetValue(Product);
@@ -121,6 +158,7 @@ namespace WpfOpenFoodFacts
                     {
                         TextBlock tb = new TextBlock();
                         tb.FontWeight = FontWeights.Bold;
+                        tb.TextWrapping = TextWrapping.Wrap;
                         tb.Text = propNL.Name + ": ";
                         wp5.Children.Add(tb);
 
